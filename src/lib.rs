1a921e73f5e7aa37a2d8b3a367c733c8332e6545
dremonkey <a.deutmeyer@gmail.com>
2015-09-12 17:20:34 -0700

    new: add dependencies and tests, passes tests

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,13 @@ use rustc_serialize::json;
 use geojson::{Feature, GeoJson, Geometry, Value};
 
 pub extern fn polygon(
-  coordinates: Vec<Vec<Vec<f64>>>) => GeoJson {
+  coordinates: Vec<Vec<Vec<f64>>>) -> GeoJson {
 
   assert!(coordinates.len() >= 1);
 
   for ring in &coordinates {
-    assert!(ring.length >= 4, "Each LinearRing of a Polygon must have 4 or more latlng");
-    assert_eq!(ring.first(), ring.last(), "First and last latlng are not equivalent")
+    assert!(ring.len() >= 4, "Each LinearRing of a Polygon must have 4 or more latlng");
+    assert_eq!(ring.first(), ring.last());
   }
 
   let geometry = Geometry::new(Value::Polygon(coordinates));
@@ -32,4 +32,35 @@ pub extern fn polygon(
     id: None,
     properties: Some(properties),
   })
+}
+
+#[cfg(test)]
+mod tests {
+  use rustc_serialize::json::{self, ToJson};
+  use super::polygon;
+
+  #[test]
+  fn test_valid_coordinates() {
+    let expected_json = "{\"geometry\":{\"coordinates\":[[[1.0,1.0],[2.0,2.0],[2.0,1.0],[1.0,1.0]]],\"type\":\"Polygon\"},\"properties\":{},\"type\":\"Feature\"}";
+
+    let coords = vec![vec![vec![1.0, 1.0], vec![2.0, 2.0], vec![2.0, 1.0], vec![1.0, 1.0]]];
+    let geojson = polygon(coords);
+    let polygon_str = json::encode(&geojson.to_json()).unwrap();
+
+    assert_eq!(polygon_str, expected_json);
+  }
+
+  #[test]
+  #[should_panic(expected = "Each LinearRing of a Polygon must have 4 or more latlng")]
+  fn test_invalid_ring_coords_count() {
+    let coords = vec![vec![vec![1.0, 1.0], vec![2.0, 2.0], vec![1.0, 1.0]]];
+    polygon(coords);
+  }
+
+  #[test]
+  #[should_panic(expected = "assertion failed: `(left == right)`")]
+  fn test_invalid_ring_coords() {
+    let coords = vec![vec![vec![1.0, 1.0], vec![2.0, 2.0], vec![2.0, 1.0], vec![3.0, 3.0]]];
+    polygon(coords);
+  }
 }
